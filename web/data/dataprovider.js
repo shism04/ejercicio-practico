0bed94617cb3d41c7f59dabc1350005ec1aa3c75
isma <sihammouismael04@gmail.com>
2024-11-08 11:43:08 +0100

    examen_hecho

--- web/data/dataprovider.js
+++ web/data/dataprovider.js
@@ -1,55 +1,13 @@
-var galeria = require("./data.json");
-var carta = require("./carta.json");
-var contactos = require("./messages.json");
-var usuarios = require("./users.json");
+var posts = require("./posts.json");
 
-function getGalleryData(){
-    return galeria;
-}
+function getAllPosts(){ return posts; }
 
-function getAllCarta(){
-    return carta;
-}
+function getPostById(id){ return posts.find(a => a.id == id); }
 
-function getItemCarta(id){
-    const item = carta.filter( (a) => { return(a.id==id)} );
-    return item[0];
-}
-
-function getAllContactos(){
-    return contactos;
-}
-
-function addContacto(nombre, email, mensaje, info){
-    contactos.push(
-        {
-            nombre: nombre,
-            email: email,
-            mensaje: mensaje,
-            fecha: new Date(),
-            info: info
-        }
-    )
-}
-
-function validateUser(email,password){
-    let u = usuarios.filter(
-        (u)=>{
-            return( (u.email==email) && (u.password==password) )
-        }
-    )
-    if(u.length>0)
-        return u[0]
-    else
-        return null
-    
-}
+function getCategories() { return [...new Set(posts.map(post => post.category))]; }
 
 module.exports = {
-    getGalleryData,
-    getAllCarta,
-    getItemCarta,
-    getAllContactos,
-    addContacto,
-    validateUser
+    getAllPosts,
+    getPostById,
+    getCategories
 }
--- web/routes/index.js
+++ web/routes/index.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var blog = require('../data/dataprovider');
+
 
 
 router.get('/',function(req,res){
-  res.render("home",{head_title:"home"})
+  var posts = blog.getAllPosts();
+  var categorias= blog.getCategories();
+  console.log(posts);
+  res.render("home",{title:"home",posts:posts,categorias:categorias})
 });
 
+router.get('/entrada/:id', function (req, res, next) {
+  const entrada = blog.getPostById(req.params.id);
+  var categorias= blog.getCategories();
+  res.render("entrada", { title: "entrada",entrada:entrada,categorias:categorias });
+});
 
 module.exports = router;
